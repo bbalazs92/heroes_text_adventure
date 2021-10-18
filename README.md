fc673d64f1086f69d860c81f041e50b4ba1dca17
Balazs <balazs.birta@gmail.com>
2021-10-18 23:20:31 +0200

    Project library added, readme updated.

---
+++ README.md
@@ -0,0 +1,2 @@
+This will be a learning project to master the Ruby programming language. At it's core, this will be a round-based text adventure game in medieval style fantasy. Features will be added based on my progress with the language. 
+
---
+++ lib/main.rb
@@ -0,0 +1,94 @@
+
+puts "Heroes: A Text Based Adventure"
+
+class Warlord
+
+  attr_reader :name, :power
+  attr_accessor :health
+
+  def initialize(name, health, power)
+    @name = name
+    @health = health
+    @power = power
+  end
+
+  def introduction
+    puts "I am #{@name}, a new challenger!"
+  end
+
+  def strike(target)
+    target.health -= self.power
+  end
+
+end
+
+class Warrior < Warlord
+
+  def battle_shout
+    puts "I am #{@name}, a fearsome warrior!"
+  end
+
+end
+
+class Paladin < Warlord
+
+  def snarky_comment
+    puts "You stand before #{@name}, Light's most powerful knight!"
+  end
+
+end
+
+class Enemy < Warlord
+
+  def threaten
+    puts "I will be your undoing!"
+  end
+
+end
+
+def print_ascii(filename)
+  File.open(filename).readlines.each do |line|
+    puts line
+  end
+end
+
+def game
+
+#create a hero
+  puts "Choose your class: Warrior or Paladin!"
+  hero_class = gets.chomp
+  if hero_class == "warrior"
+    puts "What is your name warrior?"
+    hero = Warrior.new(gets.chomp, 100, 8)
+  elsif hero_class == "paladin"
+    puts "What is your name paladin?"
+    hero = Paladin.new(gets.chomp, 100, 8)
+  else
+    puts "That is not a class!"
+  end
+
+#define round based combat
+  def battle(hero, enemy)
+    while hero.health and enemy.health > 0 do
+      puts "The hero strikes!"
+      hero.strike(enemy)
+      puts "Enemy HP left: #{enemy.health}"
+      puts "The enemy retaliates!"
+      enemy.strike(hero)
+      puts "Hero HP left: #{hero.health}"
+    end
+  end
+
+  hero.introduction
+
+  puts "#{hero.name}, as you wander into the forest, you encounter a hungry wolf. Although malnourished
+  the wolf strikes immediately as it notices you. Prepare for battle!"
+
+  enemy = Enemy.new("Wolf", 30, 4)
+
+  puts "Woof!"
+  print_ascii('wolf_ascii.txt')
+  battle(hero, enemy)
+end
+
+game
